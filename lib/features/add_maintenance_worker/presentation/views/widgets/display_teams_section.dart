306d3804ac6471fcae2d3994bbaa783f0b2343e7
Majd Al Saeed <magdmm726@gmail.com>
2024-05-24 19:36:05 +0300

    edit display_teams_section

--- lib/features/add_maintenance_worker/presentation/views/widgets/display_teams_section.dart
+++ lib/features/add_maintenance_worker/presentation/views/widgets/display_teams_section.dart
@@ -12,25 +12,76 @@ class DisplayTeamsSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<GetTeamsCubit, GetTeamsState>(
-      builder: (context, state) {
-        if (state is GetTeamsFailure) {
-          return CustomError(message: state.errorMessage);
-        } else if (state is GetTeamsSuccess) {
-          return Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) => TeamIteam(
-                name: state.teams[index].teamName ?? "",
-                id: state.teams[index].id!,
-                numberOfTeam: state.teams[index].currentWorkersCount!,
-              ),
-              itemCount: state.teams.length,
+    // return BlocBuilder<GetTeamsCubit, GetTeamsState>(
+    //   builder: (context, state) {
+    //     if (state is GetTeamsFailure) {
+    //       return CustomError(message: state.errorMessage);
+    //     } else if (state is GetTeamsSuccess) {
+    return Expanded(
+        child: Container(
+      width: 1250, // Set the desired width of the box
+      height: 300, // Set the desired height of the box
+      decoration: BoxDecoration(
+        color: Colors.white, // Set the background color
+        borderRadius: BorderRadius.circular(10.0), // Apply rounded corners
+        border: Border.all(
+          // Add the border
+          color: Colors.grey, // Set the color of the border
+          width: 2.0, // Set the thickness of the border
+        ),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 4.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Team Name",
+                  style: TextStyle(
+                      color: Colors.grey[600], fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Team ID",
+                  style: TextStyle(
+                      color: Colors.grey[600], fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Team Members Number",
+                  style: TextStyle(
+                      color: Colors.grey[600], fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Last Visit",
+                  style: TextStyle(
+                      color: Colors.grey[600], fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
-          );
-        } else {
-          return const CustomProgressIndicator();
-        }
-      },
-    );
+          ),
+          Divider(
+            color: Colors.grey,
+            height: 20,
+            thickness: 2,
+            indent: 0,
+            endIndent: 0,
+          )
+        ],
+      ),
+      // child: ListView.builder(
+      //   itemBuilder: (context, index) => TeamIteam(
+      //     name: state.teams[index].teamName ?? "",
+      //     id: state.teams[index].id!,
+      //     numberOfTeam: state.teams[index].currentWorkersCount!,
+      //   ),
+      //   itemCount: state.teams.length,
+      // ),
+    ));
+    // } else {
+    //   return const CustomProgressIndicator();
   }
+  //     },
+  //   );
+  // }
 }
