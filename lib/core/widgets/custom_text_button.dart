8774be099d38d1cc8d7b9a919610081fa2a2c9c0
Majd Al Saeed <magdmm726@gmail.com>
2024-05-24 19:28:56 +0300

    edit custom_text_button

--- lib/core/widgets/custom_text_button.dart
+++ lib/core/widgets/custom_text_button.dart
@@ -5,6 +5,7 @@ class CustomTextButton extends StatelessWidget {
   final VoidCallback onPressed;
   final TextStyle textStyle;
   final Color backGroundColor;
+  final Icon? icon;
 
   const CustomTextButton({
     super.key,
@@ -12,23 +13,35 @@ class CustomTextButton extends StatelessWidget {
     required this.onPressed,
     this.textStyle = const TextStyle(),
     this.backGroundColor = Colors.transparent,
+    this.icon,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      style: TextButton.styleFrom(
-        backgroundColor: backGroundColor,
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0),
+        style: TextButton.styleFrom(
+          backgroundColor: backGroundColor,
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
         ),
-      ),
-      onPressed: onPressed,
-      child: Text(
-        label,
-        style: textStyle,
-      ),
-    );
+        onPressed: onPressed,
+        child: Row(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          //  crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (icon != null)
+              icon!, // Use the icon! to unwrap the nullable value
+
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              label,
+              style: textStyle,
+            ),
+          ],
+        ));
   }
 }
