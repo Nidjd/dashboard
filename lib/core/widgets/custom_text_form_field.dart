9f4a778795b40f3c13a221db9cc31f3485b568bf
Majd Al Saeed <magdmm726@gmail.com>
2024-05-24 19:30:26 +0300

    edit custom_text_form_field

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class CustomTextFormFiled extends StatelessWidget {
   const CustomTextFormFiled({
     super.key,
-    required this.controller, required this.label,
+    required this.controller,
+    required this.label,
   });
 
   final TextEditingController controller;
@@ -13,11 +14,14 @@ class CustomTextFormFiled extends StatelessWidget {
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(
-        labelText: label,
-        border: const OutlineInputBorder(),
-        filled: true,
-        fillColor: Colors.teal[50],
-      ),
+          labelText: label,
+          border: OutlineInputBorder(
+              borderSide: BorderSide(
+            color: Colors.grey, // Set the color of the border
+            width: 2.0, // Set the thickness of the border
+          )),
+          filled: true,
+          fillColor: Colors.white),
       validator: (value) {
         if (value!.isEmpty) {
           return 'يرجى إدخال $label';
