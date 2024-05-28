bd9846f9e2033fc63d356813bd9ea8022b029c5e
Majd Al Saeed <magdmm726@gmail.com>
2024-05-28 16:25:51 +0300

    edit custom_text_form_field

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -5,29 +5,44 @@ class CustomTextFormFiled extends StatelessWidget {
     super.key,
     required this.controller,
     required this.label,
+    this.sizee,
+    this.option,
   });
 
   final TextEditingController controller;
   final String label;
+  final double? sizee;
+  final bool? option;
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      decoration: InputDecoration(
-          labelText: label,
-          border: OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.grey, // Set the color of the border
-            width: 2.0, // Set the thickness of the border
-          )),
-          filled: true,
-          fillColor: Colors.white),
-      validator: (value) {
-        if (value!.isEmpty) {
-          return 'يرجى إدخال $label';
-        }
-        return null;
-      },
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width * sizee!,
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+            labelText: label,
+            border: option == false
+                ? const UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                      width: 2.0,
+                    ),
+                  )
+                : const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                      width: 2.0,
+                    ),
+                  ),
+            filled: true,
+            fillColor: Colors.white),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'يرجى إدخال $label';
+          }
+        },
+      ),
     );
   }
 }
