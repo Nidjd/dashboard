4be74386190979afc4b248e7343e94845f031099
Majd Al Saeed <magdmm726@gmail.com>
2024-06-16 13:44:44 +0300

    edit home_page_body.dart

--- lib/features/home_page/presentation/views/widgets/home_page_body.dart
+++ lib/features/home_page/presentation/views/widgets/home_page_body.dart
@@ -1,11 +1,11 @@
 import 'package:dashboard/core/widgets/custom_text_button.dart';
+import 'package:dashboard/features/processes_orders/presentation/views/processes_orders.dart';
 import 'package:dashboard/features/show_teams/presentation/views/show_teams.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:dashboard/features/add_electrical/presentation/views/add_electrical.dart';
 import 'package:dashboard/features/add_maintenance_worker/presentation/views/add_maintenance_worker.dart';
-import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePageBody extends StatefulWidget {
   const HomePageBody({
@@ -21,6 +21,7 @@ class _HomePageBodyState extends State<HomePageBody> {
     const AddMaintenanceWorker(),
     const AddElectrical(),
     const ShowTeam(),
+    const ProcessesOrders(),
   ];
   int indexPage = 0;
 
@@ -38,138 +39,161 @@ class _HomePageBodyState extends State<HomePageBody> {
       child: SafeArea(
           child: Row(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8),
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              width: 300,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 6,
-                    offset: Offset(0, 2),
-                  ),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: SizedBox(
-                        width: size.width * 0.5,
-                        height: size.height * 0.3,
-                        child: Image.asset("images/logo.png"),
-                      )),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        CircleAvatar(
-                 
-                          backgroundColor: Colors.white,
-                          child: Icon(
-                            Icons.engineering,
-                            color: Colors.grey[500],
-                            size: 20,
-                          ),
+          Container(
+            width: 250,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 6,
+                  offset: Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Container(
+                      width: size.width * 0.5,
+                      height: size.height * 0.3,
+                      child: Image.asset("images/x.png"),
+                    )),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        //radius: .0,
+                        backgroundColor: Colors.white,
+                        child: Icon(
+                          Icons.engineering,
+                          color: Colors.grey[500],
+                          size: 20,
                         ),
-                        Expanded(
-                          child: CustomTextButton(
-                            backGroundColor: Colors.transparent,
-                            label: "إضافة عامل",
-                            onPressed: () => changeIndexOfPages(0),
-                            textStyle: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
+                      ),
+                      CustomTextButton(
+                        backGroundColor: Colors.transparent,
+                        label: "إضافة عامل",
+                        onPressed: () => changeIndexOfPages(0),
+                        textStyle: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                  const Divider(
-                    color: Colors.blue,
-                    height: 20,
-                    thickness: 2,
-                    indent: 0,
-                    endIndent: 0,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        CircleAvatar(
-                     
-                          backgroundColor: Colors.white,
-                          child: Icon(
-                            Icons.kitchen,
-                            color: Colors.grey[500],
-                            size: 20,
-                          ),
+                ),
+                const Divider(
+                  color: Colors.blue,
+                  height: 20,
+                  thickness: 2,
+                  indent: 0,
+                  endIndent: 0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      CircleAvatar(
+                        //radius: .0,
+                        backgroundColor: Colors.white,
+                        child: Icon(
+                          Icons.kitchen,
+                          color: Colors.grey[500],
+                          size: 20,
                         ),
-                        Expanded(
-                          child: CustomTextButton(
-                            backGroundColor: Colors.transparent,
-                            label: "إضافة جهاز الكتروني",
-                            onPressed: () => changeIndexOfPages(1),
-                            textStyle: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
+                      ),
+                      CustomTextButton(
+                        backGroundColor: Colors.transparent,
+                        label: "إضافة جهاز الكتروني",
+                        onPressed: () => changeIndexOfPages(1),
+                        textStyle: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                  const Divider(
-                    color: Colors.blue,
-                    height: 20,
-                    thickness: 2,
-                    indent: 0,
-                    endIndent: 0,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Row(
-                      children: [
-                        CircleAvatar(
-                          //radius: .0,
-                          backgroundColor: Colors.white,
-                          child: Icon(
-                            Icons.groups,
-                            color: Colors.grey[500],
-                            size: 20,
-                          ),
+                ),
+                const Divider(
+                  color: Colors.blue,
+                  height: 20,
+                  thickness: 2,
+                  indent: 0,
+                  endIndent: 0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        //radius: .0,
+                        backgroundColor: Colors.white,
+                        child: Icon(
+                          FontAwesomeIcons.recycle,
+                          color: Colors.grey[500],
+                          size: 20,
                         ),
-                        Expanded(
-                          child: CustomTextButton(
-                            backGroundColor: Colors.transparent,
-                            label: "الفرق ",
-                            onPressed: () => changeIndexOfPages(2),
-                            textStyle: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
+                      ),
+                      CustomTextButton(
+                        backGroundColor: Colors.transparent,
+                        label: " معالجة الطلبات  ",
+                        onPressed: () => changeIndexOfPages(3),
+                        textStyle: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                  const Divider(
-                    color: Colors.blue,
-                    height: 20,
-                    thickness: 2,
-                    indent: 0,
-                    endIndent: 0,
+                ),
+                const Divider(
+                  color: Colors.blue,
+                  height: 20,
+                  thickness: 2,
+                  indent: 0,
+                  endIndent: 0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        //radius: .0,
+                        backgroundColor: Colors.white,
+                        child: Icon(
+                          Icons.groups,
+                          color: Colors.grey[500],
+                          size: 20,
+                        ),
+                      ),
+                      CustomTextButton(
+                        backGroundColor: Colors.transparent,
+                        label: "الفرق ",
+                        onPressed: () => changeIndexOfPages(2),
+                        textStyle: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                const Divider(
+                  color: Colors.blue,
+                  height: 20,
+                  thickness: 2,
+                  indent: 0,
+                  endIndent: 0,
+                ),
+              ],
             ),
           ),
           Expanded(
